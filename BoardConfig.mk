b939bee2bbdc85f5d445df6f20ccaa9a7ad14ef9
IPSBHANGU <inderpreetbhangu2@gmail.com>
2022-12-18 20:12:31 +0100

    vili: adapt secure_element configs
    Signed-off-by: IPSBHANGU <inderpreetbhangu2@gmail.com>
    Change-Id: Iaac068c7ea9a79a5fea7ddd61eae2f4d2934d48c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,16 @@ TARGET_BOOTLOADER_BOARD_NAME := vili
 # Firmware
 -include vendor/xiaomi/vili-firmware/BoardConfigVendor.mk
 
+# HIDL
+ODM_MANIFEST_SKUS := \
+    vili \
+    vilijp
+
+NFC_ESE_MANIFEST := $(DEVICE_PATH)/hidl/nfc_ese.xml
+
+ODM_MANIFEST_VILI_FILES := $(NFC_ESE_MANIFEST)
+ODM_MANIFEST_VILIJP_FILES := $(NFC_ESE_MANIFEST)
+
 # Kernel
 TARGET_KERNEL_CONFIG += vendor/vili_QGKI.config
 
