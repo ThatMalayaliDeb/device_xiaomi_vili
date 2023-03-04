fbd001302c265d264ce46467797042d29a7fed2b
theOverpoweredGuy <theoverpoweredguy@gmail.com>
2023-03-04 12:34:11 +0100

    Vili: Voltage Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The VoltageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/lineage_vili.mk
+	$(LOCAL_DIR)/voltage_vili.mk
 
 COMMON_LUNCH_CHOICES := \
-	lineage_vili-user \
-	lineage_vili-userdebug \
-	lineage_vili-eng
+	voltage_vili-user \
+	voltage_vili-userdebug \
+	voltage_vili-eng
--- voltage_vili.mk
+++ voltage_vili.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022 The voltageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,14 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vili device
 $(call inherit-product, device/xiaomi/vili/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common voltage stuff.
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := vili
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2107113SG
-PRODUCT_NAME := lineage_vili
+PRODUCT_NAME := voltage_vili
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
