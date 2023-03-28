2387ec7088071733a1e1c9350609a5397d4ed1d3
theOverpoweredGuy <theoverpoweredguy@gmail.com>
2023-03-28 08:36:17 +0530

    Revert "Vili: Add Pixel Launcher Extended"
    This reverts commit 4b25c0b7bcc727919c99e69b81ceb9a6d45133ad.

--- cherish_vili.mk
+++ cherish_vili.mk
@@ -14,9 +14,6 @@ $(call inherit-product, device/xiaomi/vili/device.mk)
 # Inherit some common Cherish stuff.
 $(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
-#Inherit Pixel Launcher Extended
-$(call inherit-product, vendor/PixelLauncherExtended/PixelLauncher.mk)
-
 # Inherit some Cherish build stuff.
 TARGET_BOOT_ANIMATION_RES := 1080
 WITH_GAPPS=true
@@ -43,7 +40,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.cherish.maintainer= thatmalayali
 CHERISH_BUILD_TYPE := OFFICIAL
-
-#Adding Pixel Launcher Extended props
-PIXEL_LAUNCHER_VARIANT := fullmod2z
-ICONS_VARIANT := lawnicons
