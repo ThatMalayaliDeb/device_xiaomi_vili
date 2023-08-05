99f6e24d040c9b1b09a8a6d32a287f390d683e0f
ThatMalayaliDeb <theoverpoweredguy@gmail.com>
2023-08-06 02:13:36 +0530

    Revert "vili: Inherit MIUI Camera"
    This reverts commit 60f4ab96e6e2f513421d7b3f61c89b49cf8c40f2.

--- device.mk
+++ device.mk
@@ -21,9 +21,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.xiaomi.camera.rc
 
-# MiuiCamera
-$(call inherit-product-if-exists, vendor/xiaomi/vili-miuicamera/products/miuicamera.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
